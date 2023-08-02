f69c6644e7547641e65b3994406308b782562845
Rishikesh Bhakare <bhakarerishi@gmail.com>
2023-08-02 21:19:47 +0530

    fix: header issue on mobile

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -4,7 +4,7 @@ import { MainNav } from "./main-nav";
 import Image from "next/image";
 export function SiteHeader() {
   return (
-    <header className="sticky top-0 z-40 w-full">
+    <header className="md:sticky top-0 z-40 w-full">
       <div className="container flex h-32 md:h-20 gap-4 md:gap-0 items-center md:justify-between justify-center flex-col md:flex-row">
         <div className="md:w-44">
           <Link href="/">
