e2557cce9a6c1cfd97b2ce9a541d769e94942fd5
Rishikesh Bhakare <bhakarerishi@gmail.com>
2023-08-02 21:41:42 +0530

    fix: font issue on body

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,9 +32,14 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en" suppressHydrationWarning>
       <head />
-      <body className={cn(fontSans.variable)}>
+      <body
+        className={cn(
+          "dark:bg-[#212121] bg-[#EFEFEF] font-sans antialiased",
+          fontSans.variable
+        )}
+      >
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          <div className="relative flex min-h-screen flex-col dark:bg-[#212121] bg-[#EFEFEF] before:pointer-events-none before:absolute before:inset-0 before:block before:h-full before:w-full before:bg-[url('/background-pattern.svg')] before:bg-cover before:bg-no-repeat dark:before:opacity-10 before:opacity-20 before:grayscale font-sans antialiased">
+          <div className="relative flex min-h-screen flex-col before:pointer-events-none before:absolute before:inset-0 before:block before:h-full before:w-full before:bg-[url('/background-pattern.svg')] before:bg-cover before:bg-no-repeat dark:before:opacity-10 before:opacity-20 before:grayscale">
             <SiteHeader />
             <div className="flex-1">{children}</div>
             {/* <div className="w-full text-center py-8 z-10">
