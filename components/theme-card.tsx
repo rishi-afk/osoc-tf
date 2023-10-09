287f8e59ef499f84507346e01fd48c9cf91957ae
Rishikesh Bhakare <bhakarerishi@gmail.com>
2023-10-09 18:23:38 +0530

    ui: fixed object cover on theme card

--- components/theme-card.tsx
+++ components/theme-card.tsx
@@ -9,7 +9,7 @@ type Props = {
 export default function ThemeCard({ title, image }: Props) {
   return (
     <div className="w-full h-[400px] relative pointer">
-      <Image src={image} fill alt="member image" />
+      <Image src={image} fill alt="member image" className="object-cover" />
       <div className="w-full h-full flex items-center justify-center py-6 bg-zinc-800/80 top-0 left-0 absolute">
         <div className="flex flex-col justify-center items-center gap-2">
           <h3
